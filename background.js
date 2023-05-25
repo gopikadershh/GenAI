201c9f68c74ca4917d9104190b1f10346cf598a7
Devika <devikab2021@gmail.com>
2023-05-25 23:21:45 +0530

    api call successful

--- background.js
+++ background.js
@@ -1,53 +1,52 @@
-chrome.contextMenus.create({
-    id: 'clip',
-    title: 'BoosteX',
-    contexts: ['selection'] 
-  });
-
-  chrome.contextMenus.onClicked.addListener(
-    ({selectionText}) => {
-        console.log(selectionText)
-        var popup = document.createElement("div");
-            // Customize the popup box with your desired content and styling
-            // For example:
-            popup.textContent = "Selected Text: " + selectionText;
-            popup.style.position = "fixed";
-            popup.style.top = "50%";
-            popup.style.left = "50%";
-            popup.style.transform = "translate(-50%, -50%)";
-            popup.style.backgroundColor = "#ffffff";
-            popup.style.padding = "10px";
-            popup.style.border = "1px solid #ccc";
-            popup.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.2)";
-            
-            // Append the popup box to the document body
-            document.body.appendChild(popup);
-          });
-
 // chrome.contextMenus.create({
-//     title: "Your Extension Name",
-//     contexts: ["selection"],
+//     id: 'clip',
+//     title: 'BoosteX',
+//     contexts: ['selection'] 
 //   });
 
-//   chrome.contextMenus.onClicked.addListener(function(info, tab) {
-//     // Retrieve the selected text from the context menu info
-//     // var selectedText = info.selectionText;
-    
-//     // Create a popup box element
-//     var popup = document.createElement("div");
-//     // Customize the popup box with your desired content and styling
-//     // For example:
-//     popup.textContent = "Selected Text: ";
-//     popup.style.position = "fixed";
-//     popup.style.top = "50%";
-//     popup.style.left = "50%";
-//     popup.style.transform = "translate(-50%, -50%)";
-//     popup.style.backgroundColor = "#ffffff";
-//     popup.style.padding = "10px";
-//     popup.style.border = "1px solid #ccc";
-//     popup.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.2)";
-    
-//     // Append the popup box to the document body
-//     document.body.appendChild(popup);
-//   });
-  
+require("dotenv").config();
+async function fetchData() {
+    const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "How many people in a basketball team?",
+});
+
+console.log(completion.data.choices[0].text);
+
+ // var popup = document.createElement("div");
+        //     // Customize the popup box with your desired content and styling
+        //     // For example:
+        //     popup.textContent = "Boosted Text: " + completion.data.choices[0].text;
+        //     popup.style.position = "fixed";
+        //     popup.style.top = "50%";
+        //     popup.style.left = "50%";
+        //     popup.style.transform = "translate(-50%, -50%)";
+        //     popup.style.backgroundColor = "#ffffff";
+        //     popup.style.padding = "10px";
+        //     popup.style.border = "1px solid #ccc";
+        //     popup.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.2)";
+            
+        //     // Append the popup box to the document body
+        //     document.body.appendChild(popup);
+
+}
+
+fetchData()
+
+//   chrome.contextMenus.onClicked.addListener(
+//     ({selectionText}) => {
+//         // completion();
+//         console.log(selectionText)
+//         fetchData(selectionText)
+//     });
+
+       
+
+
